5b993655e24fffe05821292503a9f5719a3af7ff
Franco Paco <francoivanp1995@hotmail.com>
2025-07-16 20:26:08 +0200

    add exception to main file

--- src/main/Main.java
+++ src/main/Main.java
@@ -1,19 +1,23 @@
 package main;
 
 import Servicios.AppManager;
+import datos.Excepcion.TablaException;
+import datos.Excepcion.UIException;
 
 import javax.swing.*;
 
 public class Main {
 
 	public static void main(String[] args) {
+		AppManager app = new AppManager();
 		try {
-			AppManager app = new AppManager();
 			app.iniciarAplicacion();
-		} catch (Exception e) {
-			//Printeo para la consola tambien
+		}catch (TablaException e) {
 			e.printStackTrace();
-			JOptionPane.showMessageDialog(null, "Error inesperado desde el main: " + e.getMessage(), "Error general", JOptionPane.ERROR_MESSAGE);
+			System.err.println("Error al inicializar la base de datos: " + e.getMessage());
+		} catch (UIException e) {
+			e.printStackTrace();
+			System.err.println("Error al iniciar la interfaz de usuario: " + e.getMessage());
 		}
 	}
 }
